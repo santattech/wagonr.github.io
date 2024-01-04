90049d945fb65d832ee51dbc2bd6d6507d03abbc
Santanu Bhattachrya <santanu.bhattacharya@quantuminventions.com>
2024-01-04 11:48:15 +0530

    make some UI changes, Updated the JS to fix the undefined

---
+++ mileageChart.js
@@ -0,0 +1,86 @@
+// Mileage Line Chart
+const mileageChartFunc = (mileageData, labelData, labelText, placeholder) => {
+  var mileageCtx = document.getElementById(placeholder).getContext('2d');
+  var mileageChart = new Chart(mileageCtx, {
+      type: 'line',
+      options: {
+          scales: {
+              x: {
+                  display: true,
+                  ticks: {
+                      color: 'green',
+                      beginAtZero: true    
+                  }
+
+              },
+              y: {
+                  ticks: {
+                      color: 'green',
+                      beginAtZero: true
+                  }
+                  
+              }
+          },
+          backgroundColor: 'white',
+          pointBackgroundColor: 'seagreen',
+          pointRadius: .5,
+          pointHoverRadius: 4,
+          tension: 0,
+          beginAtZero: true
+      },
+      data: {
+          labels: labelData,
+          fontColor: '#73ad3f',
+          datasets: [{
+              label: labelText,
+              data: mileageData,
+              borderColor: 'rgba(75, 192, 192, 1)',
+              fontColor: 'black',
+              borderWidth: 2,
+              fill: false
+          }]
+      }
+  });
+}
+
+function convertToQuarters(dateArray) {
+  const quarterMap = {
+      '01': 1,
+      '02': 1,
+      '03': 1,
+      '04': 2,
+      '05': 2,
+      '06': 2,
+      '07': 3,
+      '08': 3,
+      '09': 3,
+      '10': 4,
+      '11': 4,
+      '12': 4
+  };
+
+  const quarterDates = {};
+
+  dateArray.forEach(dateString => {
+      const [datePart, timePart] = dateString.split(' ');
+      const [year, month, day] = datePart.split('-');
+      const quarter = quarterMap[month];
+
+      if (!quarterDates[year]) {
+      quarterDates[year] = {};
+      }
+
+      if (!quarterDates[year][quarter]) {
+      quarterDates[year][quarter] = [];
+      }
+
+      quarterDates[year][quarter].push(`${year}-${month}-${day} ${timePart}`);
+  });
+
+  return quarterDates;
+}
+
+const filteredArray = (originalArray) => {
+    const formattedArr = originalArray.filter(element => element !== null && element !== undefined && element !== "");
+    return formattedArr;  
+};
